a955b7b266aea853986b2d986fc9ae9730a4fd6a
Mohammed Arafat <mohammedarafat13579@gmail.com>
2023-10-22 01:06:45 +0600

    3 added!!

---
+++ 1000 rating/B_Xenia_and_Ringroad.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef unsigned short us;
+typedef long long ll;
+typedef string str;
+typedef double dou;
+typedef unsigned char uc;
+typedef vector<int> vi;
+typedef vector<unsigned int> vui;
+typedef vector<short> vsh;
+typedef vector<unsigned short> vus;
+typedef vector<unsigned long long> vull;
+typedef vector<long long> vll;
+typedef vector<float> vf;
+typedef vector<double> vd;
+typedef vector<char> vc;
+typedef vector<string> vs;
+#define um unordered_map
+#define umm unordered_multimap
+#define uset unordered_set
+#define umset unordered_multiset
+#define st stack
+#define qu queue
+#define pa pair
+#define pi 2*acos(0)
+#define phb(a) push_back(a)
+#define plb(a) pull_back(a)
+#define sz(v, datatype) (datatype)(v).size()
+#define all(a) (a).begin(), (a).end()
+#define allr(a) (a).rbegin(), (a).rend()
+#define endl "\n"
+#define avi(arr, n, datatype)    for (datatype i = 0; i < n; i++)\
+                                                {\
+                                                    cin >> arr[i];\
+                                                }
+#define avo(arr, n, datatype)   for (us i = 0; i < n; i++)\
+                                                {\
+                                                    cout << arr[i] << " ";\
+                                                }\
+                                            cout << endl;
+#define a2i(arr, n , m, datatype)  for(datatype i = 0; i < n; i++)\
+                                                {\
+                                                    for(datatype j = 0; j < m; j++)\
+                                                        {\
+                                                            cin >> arr[i][j];\
+                                                        }\
+                                                }
+#define a2o(arr, n, m, datatype)  for(datatype i = 0; i < n; i++)\
+                                                {\
+                                                    for(datatype j = 0; j < m; j++)\
+                                                        {\
+                                                            cout << arr[i][j] << " ";\
+                                                        }\
+                                                    cout << endl;\
+                                                }
+#define s2i(st, n, datatype)       for (datatype i = 0; i < n; ++i)\
+                                                {\
+                                                    getline(cin >> ws, st[i]);\
+                                                }
+#define s2o(st, n, datatype)      for (datatype i = 0; i < n; ++i)\
+                                                {\
+                                                    cout<<st[i]<< endl;\
+                                                }
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,m; cin>>n>>m;
+    ll s=0;
+    int a[m]; avi(a,m,int);
+    for (int i = 0; i < m-1; i++)
+    {
+        if(a[i]>a[i+1]) s+=n-(a[i]-a[i+1]);
+        else s+=a[i+1]-a[i];
+    }
+    s+=a[0]-1;
+    cout << s << endl;
+
+    return 0;
+}
